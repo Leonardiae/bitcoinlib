aa72b14eedd16c3e17400506f5ab141dc85854d1
Leonardiae <49715529+Leonardiae@users.noreply.github.com>
2020-04-24 18:00:17 +0200

    Add files via upload
    Add support for GetMempoolEntry rpc call

--- src/BitcoinLib/Responses/Bridges/ITransactionResponseMempool.cs
+++ src/BitcoinLib/Responses/Bridges/ITransactionResponseMempool.cs
@@ -5,8 +5,8 @@ namespace BitcoinLib.Responses.Bridges
 {
     //  Note: This serves as a common interface for the cases that a strongly-typed response is required while it is not yet clear whether the transaction in question is in-wallet or not 
     //  A practical example is the bridging of GetTransaction(), DecodeRawTransaction() and GetRawTransaction()
-    public interface ITransactionResponse
+    public interface ITransactionResponseMempool
     {
-        string TxId { get; set; }
+        string WTxId { get; set; }
     }
 }
