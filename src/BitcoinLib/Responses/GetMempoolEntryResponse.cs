b97543f7c3d3421293a7327c16925a209aa86dfe
Leonardiae <49715529+Leonardiae@users.noreply.github.com>
2020-04-24 18:02:21 +0200

    Add support for GetMempoolEntry RPC call

---
+++ src/BitcoinLib/Responses/GetMempoolEntryResponse.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) 2014 - 2016 George Kimionis
+// See the accompanying file LICENSE for the Software License Aggrement
+
+using System.Collections.Generic;
+using BitcoinLib.Responses.Bridges;
+using BitcoinLib.Responses.SharedComponents;
+
+namespace BitcoinLib.Responses
+{
+    public class GetMempoolEntryResponse : ITransactionResponseMempool
+    { 
+        public decimal Modifiedfee { get; set; }
+        public decimal Fee { get; set; }
+        public long Size { get; set; }
+        public string WTxId { get; set; }
+    }
+}
