83d943f4965d52014be3d8b0eed3262056c33591
Leonardiae <49715529+Leonardiae@users.noreply.github.com>
2020-04-25 10:43:47 +0200

    Add all values from getmempoolentry RPC reply
    Added all values from getmempoolentry, not sure about depends and spentby array

--- src/BitcoinLib/Responses/GetMempoolEntryResponse.cs
+++ src/BitcoinLib/Responses/GetMempoolEntryResponse.cs
@@ -9,9 +9,29 @@ namespace BitcoinLib.Responses
 {
     public class GetMempoolEntryResponse : ITransactionResponseMempool
     { 
-        public decimal Modifiedfee { get; set; }
-        public decimal Fee { get; set; }
+        public Fees fees { get; set; }
         public long vsize { get; set; }
-        public string WTxId { get; set; }
+        public long weight { get; set; }
+        public decimal fee { get; set; }
+        public decimal modifiedfee { get; set; }
+        public int height { get; set; }
+        public int descendantcount { get; set; }
+        public long descendantsize { get; set; }
+        public long descendantfees { get; set; }
+        public int ascendantcount { get; set; }
+        public long ascendantsize { get; set; }
+        public long ascendantfees { get; set; }        
+        public string wtxid { get; set; }
+        public string[] depends { get; set; }
+        public string[] spentby { get; set; }
+        public bool bip125-replaceable { get; set; }
+    }
+    
+    public class Fees
+    {
+        public decimal base { get; set; }
+        public decimal modified { get; set; }
+        public decimal ancestor { get; set; }
+        public decimal descendant { get; set; }
     }
 }
